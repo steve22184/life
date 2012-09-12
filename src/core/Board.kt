8ca60bb29e38af9d387f64f8d64adb73286e6986
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-12 14:20:00 +0400

    A board with finite array and listeners

---
+++ src/core/Board.kt
@@ -0,0 +1,92 @@
+/**
+ * @author abreslav
+ */
+package life.core
+
+import util.*
+import life.core.Liveness.*
+import java.util.ArrayList
+import java.util.LinkedHashMap
+
+class Board(width: Int, height: Int) {
+    private val matrix: MutableMatrix<Liveness> = MutableMatrixImpl(width, height) {x, y -> DEAD}
+    private val onChange: MutableMap<Any, () -> Unit> = LinkedHashMap()
+
+    val width: Int
+        get() = matrix.width
+
+    val height: Int
+        get() = matrix.height
+
+    fun get(x: Int, y: Int): Liveness = matrix[x, y]
+
+    fun set(x: Int, y: Int, value: Liveness) {
+        matrix[x, y] = value
+        notifyListeners()
+    }
+
+    fun step() {
+        val tmp = matrix.toMutableMatrix()
+        tmp.fill { x, y, v -> nextState(x, y)}
+        matrix.copyFrom(tmp)
+        notifyListeners()
+    }
+
+    fun addListener(key: Any, callback: () -> Unit) {
+        onChange[key] = callback
+    }
+
+    fun removeListener(key: Any) {
+        onChange.remove(key)
+    }
+
+    private fun nextState(x: Int, y: Int): Liveness {
+        val liveNeighbors = liveNeighbors(x, y)
+        return when(liveNeighbors) {
+            2 -> get(x, y)
+            3 -> LIVE
+            else -> DEAD
+        }
+    }
+
+    private fun liveNeighbors(x: Int, y: Int): Int = neighbors(x, y).filter {p -> get(p.x, p.y) == LIVE}.size
+
+    private fun neighbors(x: Int, y: Int): Collection<Point> {
+        val result = ArrayList<Point>()
+        for (dx in -1..1) {
+            for (dy in -1..1) {
+                if (dx != 0 || dy != 0) {
+                    val n = cell(x + dx, y + dy)
+                    if (n != null) {
+                        result.add(n)
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    private fun cell(x: Int, y: Int): Point? {
+        if (x !in 0..width - 1 || y !in 0..height - 1) return null
+        return Point(x, y)
+    }
+
+    private fun notifyListeners() {
+        for (l in onChange.values()) {
+            l()
+        }
+    }
+
+}
+
+enum class Liveness {
+    LIVE
+    DEAD
+
+    fun invert(): Liveness = when (this) {
+        DEAD -> LIVE
+        LIVE -> DEAD
+    }
+}
+
+data class Point(val x: Int, val y: Int)
