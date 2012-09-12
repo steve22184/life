687b88136edaf676d5864c1ae933c4033c52cbfd
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-12 16:24:50 +0400

    Separated to reflect the code structure in Kotlin

--- src/ui/LifeFrame.java
+++ src/ui/LifeFrame.java
@@ -1,7 +1,5 @@
 package ui;
 
-import jet.Function0;
-import jet.Tuple0;
 import life.core.Board;
 import life.core.Liveness;
 import life.core.ToroidalBoard;
@@ -10,8 +8,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.TimerTask;
 
 /**
@@ -62,43 +58,4 @@ public class LifeFrame {
         frame.setVisible(true);
     }
 
-    private static class LifeView extends JComponent {
-        private final int cellSize = 20;
-        private final Board board;
-
-        public LifeView(Board _board) {
-            this.board = _board;
-            board.addListener(this, new Function0<Tuple0>() {
-                @Override
-                public Tuple0 invoke() {
-                    repaint();
-                    return Tuple0.INSTANCE;
-                }
-            });
-
-            this.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    int x = e.getX() / cellSize;
-                    int y = e.getY() / cellSize;
-                    board.set(x, y, board.get(x, y).invert());
-                }
-            });
-
-
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            for (int y = 0; y < board.getHeight(); y++) {
-                for (int x = 0; x < board.getWidth(); x++) {
-                    g.setColor(board.get(x, y) == Liveness.LIVE ? Color.RED : Color.WHITE);
-                    g.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-                    g.setColor(Color.BLACK);
-                    g.drawRect(x * cellSize, y * cellSize, cellSize, cellSize);
-                }
-            }
-        }
-    }
 }
---
+++ src/ui/LifeView.java
@@ -0,0 +1,54 @@
+package ui;
+
+import jet.Function0;
+import jet.Tuple0;
+import life.core.Board;
+import life.core.Liveness;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+* @author abreslav
+*/
+class LifeView extends JComponent {
+    private final int cellSize = 20;
+    private final Board board;
+
+    public LifeView(Board _board) {
+        this.board = _board;
+        board.addListener(this, new Function0<Tuple0>() {
+            @Override
+            public Tuple0 invoke() {
+                repaint();
+                return Tuple0.INSTANCE;
+            }
+        });
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX() / cellSize;
+                int y = e.getY() / cellSize;
+                board.set(x, y, board.get(x, y).invert());
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int y = 0; y < board.getHeight(); y++) {
+            for (int x = 0; x < board.getWidth(); x++) {
+                g.setColor(board.get(x, y) == Liveness.LIVE ? Color.RED : Color.WHITE);
+                g.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+                g.setColor(Color.BLACK);
+                g.drawRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            }
+        }
+    }
+}
